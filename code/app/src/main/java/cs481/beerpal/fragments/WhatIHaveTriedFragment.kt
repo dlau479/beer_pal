d714c6bb90608d40a15a4ebe4c00a3cc9be8aaca
=la028 <=lau028@cougars.csusm.edu>
2022-05-10 22:57:44 -0700

    Implemented what I have tried page based off of beer user beer rating and beer id

--- code/app/src/main/java/cs481/beerpal/fragments/WhatIHaveTriedFragment.kt
+++ code/app/src/main/java/cs481/beerpal/fragments/WhatIHaveTriedFragment.kt
@@ -1,21 +1,28 @@
 package cs481.beerpal.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import cs481.beerpal.*
 import cs481.beerpal.databinding.FragmentWhatihavetriedBinding
+import cs481.beerpal.model.RatingReview
 
 class WhatIHaveTriedFragment : Fragment() {
 
+
     private var _binding: FragmentWhatihavetriedBinding? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    val wishList = mutableListOf<RatingReview>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +34,80 @@ class WhatIHaveTriedFragment : Fragment() {
 
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getData()
+    }
+
+    fun getWishList() {
+        val db = FirebaseFirestore.getInstance()
+
+
+        db.collection("ratings_reviews")
+            .get()
+            .addOnCompleteListener {
+
+                if (it.isSuccessful) {
+                    for (document in it.result!!) {
+                        var userobj = RatingReview(
+                            beer_id = document.data.getValue("beer_id").toString().toInt(),
+                            rating = document.data.getValue("rating").toString().toDouble(),
+                            review = document.data.getValue("review").toString(),
+                            user_email = document.data.getValue("user_email").toString()
+                        )
+                        wishList.add(userobj)
+                    }
+
+                }
+
+                // Log.d("Verbose","This is the size "+ this.wishList.size.toString())
+
+            }
+
+
+
+
+    }
+
+    fun getData(){
+        getWishList()
+
+        BeerRepository.getData(object : BeerRepositoryListener {
+            override fun dataListUpdated() {}
+
+            override fun dataListResult(beerList: ArrayList<Beer>) {
+                getWishList()
+                val filteredBeerList: ArrayList<Beer> = ArrayList()
+                beerList.forEach{ beer ->
+                    wishList.forEach{ id ->
+                        if (beer.id == id.beer_id.toLong() ){
+                            filteredBeerList.add(beer)
+                       }
+                        //Log.d("Verbose","This is the "+id)
+                        //Log.d("Verbose",id.toString())
+                    }
+                }
+                Log.d("Verbose","This is the size "+wishList.size.toString())
+
+                val recyclerView: RecyclerView = requireView().findViewById(R.id.home_recView)
+                recyclerView.layoutManager = LinearLayoutManager(context)
+                recyclerView.adapter = CardAdapter(filteredBeerList)
+            }
+
+        })
+
+     //   wishList.forEach{ id ->
+            // if (beer == id ){
+            //   filteredBeerList.add(beer)
+            // }
+
+     //   }
+        //Log.d("Verbose","This is the size "+wishList.size.toString())
+
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
